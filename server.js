0dfda707c86628ddd3ea30db024c754d64a356d1
saidhaidar967-source <saidhaidar967@gmail.com>
2026-08-14 00:44:09 +0300

    Updated server.js

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ const TOKEN_TTL_SECONDS = TOKEN_TTL_MINUTES * 60;
 const PRODUCTS = {
   book: {
     name: 'Dalbo Buugga',
-    price: '1',
+    price: '4.99',
     r2Key: 'siraha-ganacsi-01.pdf'
   },
   guusha: {
