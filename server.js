88b326a7c1731dcb285a3f43c49f4630c05cf310
saidhaidar967-source <saidhaidar967@gmail.com>
2026-09-06 13:39:24 +0300

    Updated server.js

--- server.js
+++ server.js
@@ -34,13 +34,13 @@ const TOKEN_TTL_SECONDS = TOKEN_TTL_MINUTES * 60;
 const PRODUCTS = {
   book: {
     name: 'Dalbo Buugga',
-    price: '4.99',
+    price: '0.5',
     r2Key: 'siraha-ganacsi-01.pdf',
     thankYouUrl: 'https://www.raadeeyenets01.co/degso-buuggaaga'
   },
   guusha: {
     name: 'Guusha Ganacsigaaga',
-    price: '3.5',
+    price: '0.5',
     r2Key: 'Guusha-Ganacsigaaga-0.pdf',
     thankYouUrl: 'https://www.raadeeyenets01.co/guusha-buug'
   },
