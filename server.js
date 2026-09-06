c3a83181c02a7cd64899e25bc5a277ba998248e0
saidhaidar967-source <saidhaidar967@gmail.com>
2026-09-06 21:45:45 +0300

    Updated server.js

--- server.js
+++ server.js
@@ -36,19 +36,16 @@ const PRODUCTS = {
     name: 'Dalbo Buugga',
     price: '0.5',
     r2Key: 'siraha-ganacsi-01.pdf',
-    thankYouUrl: 'https://www.raadeeyenets01.co/degso-buuggaaga'
   },
   guusha: {
     name: 'Guusha Ganacsigaaga',
     price: '0.5',
     r2Key: 'Guusha-Ganacsigaaga-0.pdf',
-    thankYouUrl: 'https://www.raadeeyenets01.co/guusha-buug'
   },
   billow: {
     name: 'Billow Ganacsigaaga',
     price: '0.5',
     r2Key: 'billow-ganacsigaaga.pdf',
-    thankYouUrl: 'https://www.raadeeyenets01.co/la-deg'
   }
 };
 
